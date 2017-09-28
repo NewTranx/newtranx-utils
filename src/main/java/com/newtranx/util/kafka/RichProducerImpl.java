e2b5e0ebd1a4dcc02bd7ed185a150c2e549d73e6
luyi <takamachi660@gmail.com>
2017-09-28 10:28:52 +0800

    v1.2.0: upgrade kafka to 0.11.0.1

--- src/main/java/com/newtranx/util/kafka/RichProducerImpl.java
+++ src/main/java/com/newtranx/util/kafka/RichProducerImpl.java
@@ -26,6 +26,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -33,97 +34,124 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.ProducerFencedException;
 
 class RichProducerImpl<K, V> implements RichProducer<K, V> {
 
-	public final Producer<K, V> producer;
+    public final Producer<K, V> producer;
 
-	public RichProducerImpl(Producer<K, V> producer) {
-		this.producer = producer;
-	}
+    public RichProducerImpl(Producer<K, V> producer) {
+        this.producer = producer;
+    }
 
-	public CompletableFuture<RecordMetadata> send(ProducerRecord<K, V> record, Optional<Executor> optExecutor) {
-		CompletableFuture<RecordMetadata> f = new CompletableFuture<RecordMetadata>();
-		this.producer.send(record, new Callback() {
-			@Override
-			public void onCompletion(RecordMetadata metadata, Exception exception) {
-				if (optExecutor.isPresent())
-					optExecutor.get().execute(() -> onSendCompletion(metadata, exception, f));
-				else
-					onSendCompletion(metadata, exception, f);
-			}
-		});
-		return f;
-	}
+    public CompletableFuture<RecordMetadata> send(ProducerRecord<K, V> record, Optional<Executor> optExecutor) {
+        CompletableFuture<RecordMetadata> f = new CompletableFuture<RecordMetadata>();
+        this.producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if (optExecutor.isPresent())
+                    optExecutor.get().execute(() -> onSendCompletion(metadata, exception, f));
+                else
+                    onSendCompletion(metadata, exception, f);
+            }
+        });
+        return f;
+    }
 
-	@Override
-	public CompletableFuture<List<RecordMetadata>> batchSend(List<ProducerRecord<K, V>> records,
-			Optional<Executor> optExecutor) {
-		@SuppressWarnings("unchecked")
-		CompletableFuture<RecordMetadata>[] buff = new CompletableFuture[records.size()];
-		int buffPos = 0;
-		for (ProducerRecord<K, V> record : records) {
-			buff[buffPos++] = this.send(record, optExecutor);
-		}
-		return CompletableFuture.allOf(buff).thenApply(v -> {
-			List<RecordMetadata> result = new ArrayList<>(buff.length);
-			for (CompletableFuture<RecordMetadata> f : buff) {
-				try {
-					// f.get() will success
-					result.add(f.get());
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-			}
-			return result;
-		});
-	}
+    @Override
+    public CompletableFuture<List<RecordMetadata>> batchSend(List<ProducerRecord<K, V>> records,
+                                                             Optional<Executor> optExecutor) {
+        @SuppressWarnings("unchecked")
+        CompletableFuture<RecordMetadata>[] buff = new CompletableFuture[records.size()];
+        int buffPos = 0;
+        for (ProducerRecord<K, V> record : records) {
+            buff[buffPos++] = this.send(record, optExecutor);
+        }
+        return CompletableFuture.allOf(buff).thenApply(v -> {
+            List<RecordMetadata> result = new ArrayList<>(buff.length);
+            for (CompletableFuture<RecordMetadata> f : buff) {
+                try {
+                    // f.get() will success
+                    result.add(f.get());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return result;
+        });
+    }
 
-	public Future<RecordMetadata> send(ProducerRecord<K, V> record, Callback callback) {
-		return producer.send(record, callback);
-	}
+    @Override
+    public void initTransactions() {
+        producer.initTransactions();
+    }
 
-	public List<PartitionInfo> partitionsFor(String topic) {
-		return producer.partitionsFor(topic);
-	}
+    @Override
+    public void beginTransaction() throws ProducerFencedException {
+        producer.beginTransaction();
+    }
 
-	public Map<MetricName, ? extends Metric> metrics() {
-		return producer.metrics();
-	}
+    @Override
+    public void sendOffsetsToTransaction(Map<TopicPartition, OffsetAndMetadata> offsets, String consumerGroupId) throws ProducerFencedException {
+        producer.sendOffsetsToTransaction(offsets, consumerGroupId);
+    }
 
-	@Override
-	public void close(long arg0, TimeUnit arg1) {
-		producer.close(arg0, arg1);
-	}
+    @Override
+    public void commitTransaction() throws ProducerFencedException {
+        producer.commitTransaction();
+    }
 
-	@Override
-	public void flush() {
-		producer.flush();
-	}
+    @Override
+    public void abortTransaction() throws ProducerFencedException {
+        producer.abortTransaction();
+    }
 
-	public void close() {
-		producer.close();
-	}
+    public Future<RecordMetadata> send(ProducerRecord<K, V> record, Callback callback) {
+        return producer.send(record, callback);
+    }
 
-	@Override
-	public Future<RecordMetadata> sendOldApi(ProducerRecord<K, V> record) {
-		return producer.send(record);
-	}
+    public List<PartitionInfo> partitionsFor(String topic) {
+        return producer.partitionsFor(topic);
+    }
 
-	@Override
-	public void sendSync(ProducerRecord<K, V> record) throws InterruptedException, ExecutionException {
-		producer.send(record).get();
-	}
+    public Map<MetricName, ? extends Metric> metrics() {
+        return producer.metrics();
+    }
 
-	private static void onSendCompletion(RecordMetadata metadata, Exception exception,
-			CompletableFuture<RecordMetadata> promise) {
-		if (metadata != null && exception == null) {
-			promise.complete(metadata);// success
-		} else if (exception != null && metadata == null) {
-			promise.completeExceptionally(exception);// fail
-		} else {
-			promise.completeExceptionally(new AssertionError()); // impossible
-		}
-	}
+    @Override
+    public void close(long arg0, TimeUnit arg1) {
+        producer.close(arg0, arg1);
+    }
+
+    @Override
+    public void flush() {
+        producer.flush();
+    }
+
+    public void close() {
+        producer.close();
+    }
+
+    @Override
+    public Future<RecordMetadata> sendOldApi(ProducerRecord<K, V> record) {
+        return producer.send(record);
+    }
+
+    @Override
+    public void sendSync(ProducerRecord<K, V> record) throws InterruptedException, ExecutionException {
+        producer.send(record).get();
+    }
+
+    private static void onSendCompletion(RecordMetadata metadata, Exception exception,
+                                         CompletableFuture<RecordMetadata> promise) {
+        if (metadata != null && exception == null) {
+            promise.complete(metadata);// success
+        } else if (exception != null && metadata == null) {
+            promise.completeExceptionally(exception);// fail
+        } else {
+            promise.completeExceptionally(new AssertionError()); // impossible
+        }
+    }
 
 }
